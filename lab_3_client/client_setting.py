3d31292bb56d128880c9c286443ecf8d418d9f9f
Pindrek <dimchoglo.mishelle@gmail.com>
2026-03-05 09:55:33 +0200

    Add files via upload

--- lab_3_client/client_setting.py
+++ lab_3_client/client_setting.py
@@ -1,5 +1,13 @@
 import socket
-import sys
 
-def client_setting():
-    
+def client_program():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 18003))
+
+    data = client.recv(1024)
+    print(data.decode())
+
+    client.close()
+
+if __name__ == '__main__':
+    client_program()
--- lab_3_server/Server_setting.py
+++ lab_3_server/Server_setting.py
@@ -1,6 +1,15 @@
 import socket
-import sys
 
-def server_setting():
-    host = socket.gethostname()
-    print("J")
+def server_program():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 18003))
+    server.listen(1)
+
+    conn, addr = server.accept()
+    conn.send(b"Hello man")
+
+    conn.close()
+    server.close()
+
+if __name__ == '__main__':
+    server_program()
